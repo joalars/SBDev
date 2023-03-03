7611695dfea4af10dca88273c17e0a9def758d58
Rayus <3180622+joalars@users.noreply.github.com>
2023-03-03 19:44:25 +0100

    new file:   .gitignore 	new file:   README.md 	new file:   aftman.toml 	new file:   default.project.json 	new file:   src/clientCharacter/Jetpack.lua 	new file:   src/server/JetServer.lua

---
+++ src/clientCharacter/Jetpack.lua
@@ -0,0 +1,286 @@
+print("Jetpack - Local")
+
+local uis = game:GetService("UserInputService")
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
+local plr = game:GetService("Players").LocalPlayer
+
+local speed = 16
+local maxSpeed = 64
+
+local increasing = false
+local decreasing = false
+local changed = false
+local turbo = false
+
+local inctrg = os.clock()
+local inc = os.clock()
+
+local onGround = true
+local flying = false
+
+local hovering = false
+local hoverPoint = nil
+
+local cd = 0.05
+
+local p,s
+
+local attach = Instance.new("Attachment")
+attach.Parent = plr.Character.HumanoidRootPart
+attach.CFrame = CFrame.new(0,0,0)
+
+local alignOrientation = Instance.new("AlignOrientation",plr.Character.HumanoidRootPart)
+alignOrientation.Attachment0 = attach
+alignOrientation.AlignType = Enum.AlignType.Parallel
+alignOrientation.Mode = Enum.OrientationAlignmentMode.OneAttachment
+alignOrientation.Responsiveness = 15
+
+local alignPosition = Instance.new("AlignPosition",plr.Character.HumanoidRootPart)
+alignPosition.Attachment0 = attach
+alignPosition.Mode = Enum.PositionAlignmentMode.OneAttachment
+alignPosition.Enabled = false
+alignPosition.MaxForce = math.huge
+alignPosition.MaxVelocity = speed
+
+function sendServerEvent(t,p,a)
+	local event = replicatedStorage:FindFirstChild("jetpackEvent")
+	if event then
+		event:FireServer(t,p,a)
+	else
+		warn("[Jetpack Local]: Event not found")
+	end
+end
+
+uis.InputBegan:Connect(function(input, gameProcessed)
+	if gameProcessed == false then
+		if input.UserInputType == Enum.UserInputType.Keyboard then
+			if input.KeyCode == Enum.KeyCode.R then
+				--print("+R")
+				increasing = true
+			elseif input.KeyCode == Enum.KeyCode.F then
+				--print("+F")
+				decreasing = true
+			elseif input.KeyCode == Enum.KeyCode.Space then
+				--print("+Space")
+				if onGround == false then
+					flying = true
+					sendServerEvent("Flying",p,true)
+					--print("+Flying")
+				end
+			elseif input.KeyCode == Enum.KeyCode.T then
+				if flying == true and hovering == false then
+					turbo = true
+					sendServerEvent("Turbo",p,true)
+				end
+			end
+		end
+	end
+end)
+
+uis.InputEnded:Connect(function(input,gameProcessed)
+	if input.UserInputType == Enum.UserInputType.Keyboard then
+		if input.KeyCode == Enum.KeyCode.R then
+			--print("-R")
+			increasing = false
+		elseif input.KeyCode == Enum.KeyCode.F then
+			--print("-F")
+			decreasing = false
+		elseif input.KeyCode == Enum.KeyCode.Space then
+			--print("-Space")
+			if flying == true then
+				flying = false
+				turbo = false
+				sendServerEvent("Flying",p,false)
+				sendServerEvent("Turbo",p,false)
+				plr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+				--print("-Flying")
+			end
+		elseif input.KeyCode == Enum.KeyCode.T then
+			turbo = false
+			sendServerEvent("Turbo",p,false)
+		end
+	end
+end)
+
+function findJetpackComps(plr)
+	local chr = plr.Character
+	if chr then
+		local pack = chr:FindFirstChild("Jetpack")
+		if pack then
+			local sgui = pack:FindFirstChildOfClass("SurfaceGui")
+			if sgui then
+				local bgfrm = sgui:FindFirstChildOfClass("Frame")
+				if bgfrm then
+					local spfrm = bgfrm:FindFirstChildOfClass("Frame")
+					if spfrm then
+						return pack,spfrm
+					end
+				end
+			end
+			return pack,nil
+		end
+	end
+	return nil
+end
+
+local died
+local rs
+died = plr.Character.Humanoid.Died:Connect(function()
+	died:Disconnect()
+	rs:Disconnect()
+end)
+
+local stunned = false
+stunnedFrames = 0
+stunDuration = 2.5
+
+local f = 0
+rs = game:GetService("RunService").RenderStepped:Connect(function(delta)
+
+	f = f + 1
+	
+	if stunned == true then
+		stunnedFrames = stunnedFrames + delta
+	end
+
+	inc = inc + delta
+
+	--print(inc - inctrg)
+
+	if onGround and flying == true then
+		flying = false
+		turbo = false
+		sendServerEvent("Flying",p,false)
+		sendServerEvent("Turbo",p,false)
+	end
+
+	if flying == true then
+		if speed > 2 then
+			plr.Character.Humanoid.PlatformStand = true
+			alignOrientation.Enabled = true
+			alignPosition.Enabled = true
+			hovering = false
+			hoverPoint = nil
+		else
+			plr.Character.Humanoid.PlatformStand = true
+			alignOrientation.Enabled = true
+			alignPosition.Enabled = true
+			plr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+			hovering = true
+			if hoverPoint == nil then
+				hoverPoint = plr.Character.HumanoidRootPart.Position
+				if turbo == true then
+					turbo = false
+					sendServerEvent("Turbo",p,false)
+				end
+			end
+		end
+	else
+		hovering = false
+		hoverPoint = nil
+		plr.Character.Humanoid.PlatformStand = false
+		alignOrientation.Enabled = false
+		alignPosition.Enabled = false
+	end
+
+	alignOrientation.CFrame = workspace.CurrentCamera.CFrame.Rotation * CFrame.Angles(-math.pi/2,0,0)
+
+	if hovering == false then
+		alignPosition.Position = plr.Character.HumanoidRootPart.CFrame.p + plr.Character.HumanoidRootPart.CFrame.UpVector * 100
+	else
+		alignPosition.Position = hoverPoint + Vector3.new(math.sin(f*0.003)*1.5,math.cos(f*0.001)*1.5,math.sin(f*0.002)*1.5)
+		alignOrientation.CFrame = workspace.CurrentCamera.CFrame.Rotation * CFrame.Angles(0,0,0)
+	end
+
+	if turbo == false then
+		alignPosition.MaxVelocity = speed
+		alignOrientation.RigidityEnabled = false
+	else
+		alignPosition.MaxVelocity = maxSpeed * 4
+		alignOrientation.RigidityEnabled = true
+	end
+
+	if inctrg <= inc then
+		inctrg = inc + cd
+		--print("SendingTriggered")
+		p,s = findJetpackComps(plr)
+		if p then
+			--print(changed)
+			if changed == true then
+				--print("UPDATED")
+				sendServerEvent("Speed",p,-speed/maxSpeed)
+				changed = false
+			end
+		end
+	end
+
+	if increasing == true then
+		speed = speed + delta * 35
+		changed = true
+		if speed > maxSpeed then
+			speed = maxSpeed
+		end
+	end
+	if decreasing == true then
+		speed = speed - delta * 35
+		changed = true
+		if speed < 1 then
+			speed = 1
+		end
+	end
+
+	if p then
+		if s then
+			s.Size = UDim2.new(1,0,-speed/maxSpeed,0)
+		end
+	end
+
+	if hovering == false then
+		
+		local rayParams = RaycastParams.new()
+		rayParams.FilterDescendantsInstances = {plr.Character}
+		rayParams.FilterType = Enum.RaycastFilterType.Exclude
+		rayParams.IgnoreWater = true
+		rayParams.RespectCanCollide = true
+		
+		if flying == false then
+			local rayResult = workspace:Raycast(plr.Character.HumanoidRootPart.CFrame.p, Vector3.new(0, -4.5, 0), rayParams)
+			if rayResult then
+				onGround = true
+			else
+				onGround = false
+			end
+		else
+			local rayResult = workspace:Raycast(plr.Character.HumanoidRootPart.CFrame.p, plr.Character.HumanoidRootPart.CFrame.UpVector*4.5, rayParams)
+			if rayResult then
+				onGround = true
+				flying = false
+				turbo = false
+				hovering = false
+				sendServerEvent("Flying",p,false)
+				sendServerEvent("Turbo",p,false)
+				stunned = true
+				stunnedFrames = 0
+			else
+				onGround = false
+			end
+		end
+	end
+	
+	if stunned == true and stunnedFrames < stunDuration then
+		plr.Character.Humanoid.PlatformStand = true
+	elseif stunned == true and stunnedFrames >= stunDuration then
+		stunned = false
+		plr.Character.Humanoid.PlatformStand = false
+		plr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
+	end
+end)
+
+repeat task.wait() until replicatedStorage:FindFirstChild("jetpackEvent")
+
+sendServerEvent("Generate")
+
+repeat task.wait() until plr.Character:FindFirstChild("Jetpack")
+
+p,s = findJetpackComps(plr)
---
+++ src/server/JetServer.lua
@@ -0,0 +1,277 @@
+script.Parent = __jetpack
+print("Jetpack - Server")
+
+local plr
+local replicatedStorage = game:GetService("ReplicatedStorage")
+
+repeat
+	for i,v in pairs(game:GetService("Players"):GetChildren()) do
+		plr = v
+	end
+	task.wait()
+until plr ~= nil and plr.Character
+
+function rct(a)
+	--print("[RCT]: Processing "..a:GetFullName())
+	for i,v in pairs(a:GetChildren()) do
+		--print("[RCT]: Scanning - "..v.Name)
+		if (v.Name == "UpperTorso" or v.Name == "Torso") and v:IsA("BasePart") then
+			print("[RCT]: Successfully Identified: "..v.Name)
+			return v
+		end
+	end
+	print("[RCT]: Search failed. Invalid character.")
+	return nil
+end
+
+function ins(t,vars)
+	local i = Instance.new(t)
+	local errored
+	for _,v in pairs(vars) do
+		xpcall(function()
+			i[v[1]] = v[2]
+		end,function()
+			warn("[INS]: Errored at ("..t..") while setting variable ("..v[1]..")")
+		end)
+	end
+	return i
+end
+
+function weld(a,b,c0,c1)
+	local w = ins("Weld",{
+		{"Part0",a},
+		{"Part1",b},
+		{"C0",c0 or CFrame.new(0,0,0)},
+		{"C1",c1 or CFrame.new(0,0,0)},
+		{"Name","Weld: "..a.Name.." - "..b.Name}
+	})
+	return w
+end
+
+function generate(plr)
+
+	for i,v in pairs(plr.Character:GetChildren()) do
+		if v.Name == "Jetpack" then
+			v:Destroy()
+		end
+	end
+
+	local trs = rct(plr.Character)
+
+	local container = ins("Model",{
+		{"Name","Jetpack"},
+		{"Parent",trs.Parent}
+	})
+
+	local p1 = ins("Part", {
+		{"Size",Vector3.new(1.25,1.7,0.2)},
+		{"Position",Vector3.new(0,5,0)},
+		{"Anchored",false},
+		{"CanCollide",false},
+		{"TopSurface",Enum.SurfaceType.Smooth},
+		{"BottomSurface",Enum.SurfaceType.Smooth},
+		{"Material",Enum.Material.SmoothPlastic},
+		{"Parent",container}
+	})
+
+	local p2 = ins("Part", {
+		{"Shape",Enum.PartType.Cylinder},
+		{"Size", Vector3.new(1.7,0.2,0.2)},
+		{"CanCollide",false},
+		{"Material",Enum.Material.SmoothPlastic},
+		{"TopSurface",Enum.SurfaceType.Smooth},
+		{"BottomSurface",Enum.SurfaceType.Smooth},
+		{"Parent", container}
+	})
+
+	local p3 = p2:Clone()
+	p3.Parent = container
+	
+	local p4 = ins("Part",{
+		{"Size",Vector3.new(0.2,0.2,0.2)},
+		{"Transparency",1},
+		{"CanCollide",false},
+		{"Parent",container}
+	})
+	
+	local p5 = ins("Part",{
+		{"Size",Vector3.new(0.2,0.2,0.2)},
+		{"Transparency",1},
+		{"CanCollide",false},
+		{"Parent",container}
+	})
+	
+	local f1 = ins("Fire",{
+		{"Size",0.175},
+		{"Enabled",false},
+		{"Parent",p4}
+	})
+	
+	local f2 = ins("Fire",{
+		{"Size",0.175},
+		{"Enabled",false},
+		{"Parent",p5}
+	})
+	
+	local song = ins("Sound",{
+		{"SoundId","rbxassetid://9044545570"}, --SONG
+		{"Looped",true},
+		{"Name","Song"},
+		{"Parent",p1}
+	})
+	
+	local tsound = ins("Sound",{
+		{"SoundId","rbxassetid://12659320869"},
+		{"Looped",true},
+		{"Name","Thruster"},
+		{"Pitch",((1/64)*0.25)*20 + 0.5},
+		{"Parent",p1}
+	})
+
+	local w1 = weld(trs,p1,CFrame.new(0,0,0.7),CFrame.new(0,0,0))
+	w1.Parent = p1
+
+	local w2 = weld(p1,p2,CFrame.new(1.25/2,0,0),CFrame.new(0,0,0) * CFrame.Angles(0,0,math.pi/2))
+	w2.Parent = p2
+
+	local w3 = weld(p1,p3,CFrame.new(-1.25/2,0,0),CFrame.new(0,0,0) * CFrame.Angles(0,0,math.pi/2))
+	w3.Parent = p3
+	
+	local w4 = weld(p1,p4,CFrame.new(-1.25/2,-1.7/2,0),CFrame.new(0,0,0) * CFrame.Angles(math.pi,0,0))
+	w4.Parent = p4
+	
+	local w5 = weld(p1,p5,CFrame.new(1.25/2,-1.7/2,0),CFrame.new(0,0,0) * CFrame.Angles(math.pi,0,0))
+	w5.Parent = p5
+
+	local display = ins("SurfaceGui",{
+		{"Name","Display"},
+		{"Adornee",p1},
+		{"Face","Back"},
+		{"Parent",container}
+	})
+
+	local speedBG = ins("Frame",{
+		{"Name","SpeedBackground"},
+		{"Size",UDim2.new(0.05,0,1,-25)},
+		{"Position",UDim2.new(0,0,0,12.5)},
+		{"BackgroundColor3",Color3.new(0,0,0)},
+		{"BorderSizePixel",0},
+		{"Parent",display}
+	})
+
+	local speedDis = ins("Frame",{
+		{"Name","SpeedDisplay"},
+		{"Size",UDim2.new(1,0,-0.25,0)},
+		{"Position",UDim2.new(0,0,1,0)},
+		{"BackgroundColor3",Color3.new(1,1,1)},
+		{"BorderSizePixel",0},
+		{"Parent",speedBG}
+	})
+end
+
+local jetpackEvent
+
+local eventHandler
+
+function restoreEvent()
+	if jetpackEvent == nil then
+		jetpackEvent = ins("RemoteEvent",{
+			{"Name","jetpackEvent"},
+			{"Parent",replicatedStorage}
+		})
+		if eventHandler then
+			eventHandler:Disconnect()
+		end
+		eventHandler = jetpackEvent.OnServerEvent:Connect(function(plr,t,p,a)
+			xpcall(function()
+				--print("Message received")
+				--print(t)
+				print("Signal: "..t)
+				if t == "Generate" then
+					generate(plr)
+				end
+				if t == "Speed" then
+					if p.Parent == plr.Character then
+						p.Display.SpeedBackground.SpeedDisplay.Size = UDim2.new(1,0,a,0)
+						for i,v in pairs(p:GetChildren()) do
+							local f = v:FindFirstChild("Thruster")
+							if f then
+								f.Pitch = ((1/64)*-a)*20 + 0.5
+							end
+						end
+					else
+						error()
+					end
+				end
+				if t == "Flying" then
+					print("Flying signal")
+					if p.Parent == plr.Character then
+						print("Valid player")
+						if a == true then
+							print("True")
+							for i,v in pairs(p:GetChildren()) do
+								local f = v:FindFirstChild("Fire")
+								if f then
+									print("Fire")
+									f.Enabled = true
+								end
+								local f = v:FindFirstChild("Thruster")
+								if f then
+									f:Play()
+								end
+							end
+						else
+							print("False")
+							for i,v in pairs(p:GetChildren()) do
+								local f = v:FindFirstChild("Fire")
+								if f then
+									print("Fire")
+									f.Enabled = false
+								end
+								local f = v:FindFirstChild("Thruster")
+								if f then
+									f:Stop()
+								end
+							end
+						end
+					end
+				end
+				if t == "Turbo" then
+					if p.Parent == plr.Character then
+						if a == true then
+							for i,v in pairs(p:GetChildren()) do
+								local f = v:FindFirstChild("Song")
+								if f then
+									f:Play()
+								end
+							end
+						else
+							for i,v in pairs(p:GetChildren()) do
+								local f = v:FindFirstChild("Song")
+								if f then
+									f:Stop()
+								end
+							end
+						end
+					end
+				end
+			end,function()
+				warn("[EventHandler]: Faulty request received, tossing.")
+			end)
+		end)
+	elseif jetpackEvent.Parent ~= replicatedStorage then
+		jetpackEvent.Parent = replicatedStorage
+	elseif jetpackEvent.Name ~= "jetpackEvent" then
+		jetpackEvent.Name = "jetpackEvent"
+	end
+end
+
+local temp = game.ReplicatedStorage:FindFirstChild("jetpackEvent")
+if temp then
+	temp:Destroy()
+	temp = nil
+end
+
+while task.wait() do
+	restoreEvent()
+end
